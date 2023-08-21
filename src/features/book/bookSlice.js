e5624b14abdec4fcd0d7a7f15d5fd02a958f633c
prg-04 <karanjaevans95@gmail.com>
2023-08-21 18:56:44 +0300

    Add redux store

--- src/features/book/bookSlice.js
+++ src/features/book/bookSlice.js
@@ -1,14 +1,22 @@
-import { createSlice } from '@reduxjs/toolkit';
-import books from '../../constants/constants';
+import { createSlice } from "@reduxjs/toolkit";
+import books from "../../constants/constants";
 
 const initialState = books;
 
 const bookSlice = createSlice({
-  name: 'book',
+  name: "book",
   initialState,
   reducers: {
     addBook: (state, action) => {
       state.push(action.payload);
+    },
+
+    removeBook: (state, action) => {
+      const index = state.findIndex((book) => book.id === action.payload.id);
+
+      if (index !== -1) {
+        state.splice(index, 1);
+      }
     },
   },
 });
