9b1ad546fd0a47d6b6097584b7c2b47cea9e2fbf
prg-04 <karanjaevans95@gmail.com>
2023-08-25 10:11:59 +0300

    Add async logic

--- src/redux/book/bookSlice.js
+++ src/redux/book/bookSlice.js
@@ -1,25 +1,56 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
 import books from '../../constants/constants';
 
 const initialState = books;
+let id;
+
+const baseUrl = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/';
+
+export const getId = async () => {
+  const response = await axios.post(baseUrl);
+  return response.data;
+};
+
+export const getBooks = createAsyncThunk('/book/getBooks', async () => {
+  id = await getId();
+  const res = axios.get(`${baseUrl}/${id}/books`);
+  return res.data;
+});
+
+export const addBook = createAsyncThunk('/book/addBook', async (bookData) => {
+  id = await getId();
+  const post = await axios.post(`${baseUrl}/${id}/books`, {
+    item_id: bookData.id,
+    title: bookData.title,
+    author: bookData.author,
+    category: bookData.category,
+  });
+
+  return post.data;
+});
+
+export const removeBook = createAsyncThunk(
+  '/book/removeBook',
+  async (bookId) => {
+    const deleteBook = await axios.delete(`${baseUrl}/${id}/books/${bookId}`);
+    return deleteBook.data;
+  },
+);
 
 const bookSlice = createSlice({
   name: 'book',
   initialState,
-  reducers: {
-    addBook: (state, action) => {
-      state.push(action.payload);
-    },
-
-    removeBook: (state, action) => {
-      const index = state.findIndex((book) => book.id === action.payload.id);
-
-      if (index !== -1) {
-        state.splice(index, 1);
-      }
-    },
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(getBooks.pending, () => {})
+      .addCase(getBooks.fulfilled, (action) => action.payload)
+      .addCase(getBooks.rejected, () => {})
+      .addCase(addBook.pending, () => {})
+      .addCase(addBook.fulfilled, () => {})
+      .addCase(addBook.rejected, () => {});
   },
 });
 
-export const { addBook, removeBook } = bookSlice.actions;
 export default bookSlice.reducer;
